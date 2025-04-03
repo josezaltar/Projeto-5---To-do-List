8d0e32728fe381d9558b983d764054419e0fec0a
zezaltar <josezaltar@gmail.com>
2025-04-03 14:37:12 -0300

    Paramentrizando os filtros

--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -2,9 +2,11 @@ import styled from 'styled-components'
 
 import { Props } from '.'
 
-export const Card = styled.div<Props>`
+type PropsSemLegendaEContador = Omit<Props, 'legenda' | 'contador'>
+
+export const Card = styled.div<PropsSemLegendaEContador>`
   padding: 8px;
-  border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#a1a1a1')}
+  border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#a1a1a1')};
   background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')};
   color: ${(props) => (props.ativo ? '#1E90FF' : '#5e5e5e')};
   border-radius: 8px;
--- src/containers/BarraLateral/styles.ts
+++ src/containers/BarraLateral/styles.ts
@@ -14,7 +14,7 @@ export const Filtros = styled.div`
 `
 
 export const Campo = styled.input`
-  padding: 8px:
+  padding: 8px;
   background-color: #fff;
   border-radius: 8px;
   font-weight: bold;
