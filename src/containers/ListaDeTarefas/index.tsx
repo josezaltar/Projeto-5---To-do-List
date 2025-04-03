c57a362348c20720852153b4df6ebe5353cba213
zezaltar <josezaltar@gmail.com>
2025-04-03 15:18:37 -0300

    Criando o card de tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,28 @@
+import Tarefa from '../../components/Tarefa'
+
+import * as S from './styles'
+
 const ListaDeTarefas = () => (
-  <main>
+  <S.Container>
     <p>2 Tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
-  </main>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
+  </S.Container>
 )
 
 export default ListaDeTarefas
