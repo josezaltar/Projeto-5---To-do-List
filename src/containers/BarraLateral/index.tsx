06c824852393d5ec990f396fe22275701bd52953
zezaltar <josezaltar@gmail.com>
2025-04-10 14:27:18 -0300

    Filtrando tarefas

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,8 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
+import { RootReducer } from '../../store'
+import { alterarTermo } from '../../store/reducers/filtro'
 
 import * as S from './styles'
-import { RootReducer } from '../../store'
+import * as enums from '../../utils/enums/Tarefa'
+
 const BarraLateral = () => {
   const dispatch = useDispatch()
   const { termo } = useSelector((state: RootReducer) => state.filtro)
@@ -14,17 +17,35 @@ const BarraLateral = () => {
           type="text"
           placeholder="Buscar"
           value={termo}
-          onChange={(e) =>
-            dispatch({ type: 'filtro/alterarTermo', payload: e.target.value })
-          }
+          onChange={(e) => dispatch(alterarTermo(e.target.value))}
         />
         <S.Filtros>
-          <FiltroCard legenda="pendentes" contador={1} />
-          <FiltroCard legenda="concluídas" contador={2} />
-          <FiltroCard legenda="urgentes" contador={3} />
-          <FiltroCard legenda="importantes" contador={4} />
-          <FiltroCard legenda="normal" contador={5} />
-          <FiltroCard legenda="Todas" contador={10} ativo />
+          <FiltroCard
+            valor={enums.Status.PENDENTE}
+            criterio="status"
+            legenda="pendentes"
+          />
+          <FiltroCard
+            valor={enums.Status.CONCLUIDA}
+            criterio="status"
+            legenda="concluídas"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="prioridade"
+            legenda="urgentes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.IMPORTANTE}
+            criterio="prioridade"
+            legenda="importantes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.NORMAL}
+            criterio="prioridade"
+            legenda="normal"
+          />
+          <FiltroCard criterio="todas" legenda="Todas" />
         </S.Filtros>
       </div>
     </S.Aside>
